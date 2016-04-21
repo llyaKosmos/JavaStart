d20fae40ae52cc04542bade7a83a59b59e916014
Ilya <kosmos354000@gmail.com>
2016-04-21 21:21:52 +0300

    Изучение Jaava. "Привет, Мир!"

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * A program that prints <code>Hello world!</code>
+ *
+ * @author Prog
+ * @version 1.0
+ */
+public class HelloWorld {
+    /**
+     * Program entry point
+     * @param cmdLineArgs
+     */
+    public static void main(String[] cmdLineArgs) {
+        System.out.println("Hello World!");
+    }
+}
